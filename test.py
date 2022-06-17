9dda4da94e2504b701000139d35c90b67dd4e339
kikuzo uchikoga <zosan@zosan-tower.lab>
2022-06-17 17:20:58 +0900

    train&test

--- test.py
+++ test.py
@@ -13,13 +13,18 @@ from sklearn.preprocessing import MinMaxScaler
 path = "./Dataset/"
 mmscaler = MinMaxScaler(feature_range=(-1, 1), copy=True)
 
-ax = mmscaler.fit_transform(np.loadtxt(path + "train/Inertial Signals/total_acc_x_train.txt"))
-ay = mmscaler.fit_transform(np.loadtxt(path + "train/Inertial Signals/total_acc_y_train.txt"))
-az = mmscaler.fit_transform(np.loadtxt(path + "train/Inertial Signals/total_acc_z_train.txt"))
+#training set
+train_ax = mmscaler.fit_transform(np.loadtxt(path + "train/Inertial Signals/total_acc_x_train.txt"))
+train_ay = mmscaler.fit_transform(np.loadtxt(path + "train/Inertial Signals/total_acc_y_train.txt"))
+train_az = mmscaler.fit_transform(np.loadtxt(path + "train/Inertial Signals/total_acc_z_train.txt"))
 
-t = np.loadtxt(path + "train/y_train.txt")
-s = np.loadtxt(path + "train/subject_train.txt")
+train_t = np.loadtxt(path + "train/y_train.txt")
+train_s = np.loadtxt(path + "train/subject_train.txt")
 
-print(ax)
-print(t)
-print(s)
+#test set
+test_ax = mmscaler.fit_transform(np.loadtxt(path + "test/Inertial Signals/total_acc_x_test.txt"))
+test_ay = mmscaler.fit_transform(np.loadtxt(path + "test/Inertial Signals/total_acc_y_test.txt"))
+test_az = mmscaler.fit_transform(np.loadtxt(path + "test/Inertial Signals/total_acc_z_test.txt"))
+
+test_t = np.loadtxt(path + "test/y_test.txt")
+test_s = np.loadtxt(path + "test/subject_test.txt")
