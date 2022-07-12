dbf7ee08550ede08a1a8bc3bb9f909d95de4e1c1
awesome548 <kiku.uchikoga@gmail.com>
2022-07-12 19:14:52 +0900

    データ取得できます

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from flask import Flask,request,jsonify,render_template
-import predict
+from flask import Flask,jsonify,render_template,request
+#import predict
 import numpy as np
 
 app = Flask(__name__)
@@ -10,13 +10,18 @@ def index():
 
 @app.route('/post', methods=['POST'])
 def post():
-    req = request.json
-    x = np.array(req["x"])
-    y = np.array(req["y"])
-    z = np.array(req["z"])
-    data = np.vstack(x,y,z)
-    # result = predict.main(data)
-    return jsonify({'result':data})
+    data = request.json
+    
+    # print(data)
+    # print(type(data))
+    print(data["x"])
+
+    return jsonify(data)
+    
+    # x = np.array(req["x"])
+    # y = np.array(req["y"])
+    # z = np.array(req["z"])
+    # data = np.vstack(x,y,z)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
